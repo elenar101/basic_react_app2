f167570fc363c264e790c03a4489f9239497fa2c
elenar101 <66783373+elenar101@users.noreply.github.com>
2020-06-14 21:33:37 +0300

    Updates

---
+++ precache-manifest.d0267309bcbebfea19763db4798b64d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e750cd5f4e2d893752166d0dbbf551b1",
+    "url": "/basic_react_app/index.html"
+  },
+  {
+    "revision": "8306ad3f94610bb31ba2",
+    "url": "/basic_react_app/static/css/main.5541ed6f.chunk.css"
+  },
+  {
+    "revision": "14ecfcf024b49afcef82",
+    "url": "/basic_react_app/static/js/2.129706b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic_react_app/static/js/2.129706b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8306ad3f94610bb31ba2",
+    "url": "/basic_react_app/static/js/main.ff0eaa32.chunk.js"
+  },
+  {
+    "revision": "fa0073693eb68b6554f8",
+    "url": "/basic_react_app/static/js/runtime-main.4833f679.js"
+  },
+  {
+    "revision": "824725ca5c508101fe53c63e71f6c7b3",
+    "url": "/basic_react_app/static/media/main-background.824725ca.jpg"
+  }
+]);
